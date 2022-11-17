e11b233ed8aa6f1cf16a7647a2088abdd909cc87
Lee Dano <eksh7080@gmail.com>
2022-11-18 02:09:16 +0900

    Start Retflix

---
+++ src/Row/index.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios.js";
+import { Container } from "./style";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+const Row = ({title, fetchUrl, isLargeRow}) =>{
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            
+
+            setMovies(request.data.results);
+            console.log(request)
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
+
+    return (
+        <Container>
+            <h2>{title}</h2>
+  
+            <div className="row__posters">
+                {movies.map((movie) => (
+                    <img
+                        key={movie.id}
+                        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                        alt={movie.name}
+                    />
+                ))}
+            </div>
+      </Container>
+  
+    )
+}
+
+
+
+export default Row;
---
+++ src/pages/Main/style.js
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const MainContainer = styled.section`
+    max-width: 100%;
+    display: grid;
+
+
+
+
+`
