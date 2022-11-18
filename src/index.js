f64686cfa7d8a7634801a42b723f95be75e3634d
dongjooyun <dongjooyun@khu.ac.kr>
2022-11-18 20:29:07 +0900

    Update: copied previous repo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from "pages/Login";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+
+  return (
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route exact path="/" element={<Login />} /> {/* => 로그인페이지 */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/login/style.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { MainContainer } from "./style";
+
+const Main = () => {
+
+    return (
+        <MainContainer>
+
+
+        </MainContainer>
+    )
+}
+
+export default Main;
