c9d96a88b33790163f83a51055d9575c1b46c26a
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2022-11-19 12:33:12 +0900

    Update: Footer 추가

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import Header from 'components/Header';
+import Footer from 'components/Footer';
 import React from 'react';
 
 const Main = () => {
@@ -6,6 +7,7 @@ const Main = () => {
         <>
             <Header />
             <h1>HelloWorld</h1>
+            <Footer />
         </>
     );
 };
