9dba60a8b4563220edb6c4eb27ab8490d463021f
dongjooyun <dongjooyun@khu.ac.kr>
2022-11-21 21:54:00 +0900

    Feat: structure set

---
+++ src/components/LoginHeader/style.js
@@ -0,0 +1,19 @@
+//LoginHeader
+import styled from "@emotion/styled";
+export const HeaderStyle = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 15px;
+    margin-top: 2px;
+    margin-left: 55px;
+
+    .item{
+        display: flex;
+        align-item: center;
+
+    }
+    .item.logo{
+        justify-content: flex-start;
+    }
+`;
+
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { LoginContainer } from "./style";
+import loginBG from 'assets/images/KR_login_bg.jpg';
+import LoginHeader from 'components/LoginHeader';
+import Footer from "components/Footer";
 
 const Login = () => {
 
     return (
         <LoginContainer>
-
-
+            <LoginHeader />
+            <div>
+                <article>
+                    <img src={loginBG} alt="loginBG" />
+                </article>
+            </div>
+            <Footer />
         </LoginContainer>
     )
 }
