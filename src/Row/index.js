04f721fc00939954ab51bc10b535ad8ec9cdfa90
홍희선 <hia990618@gmail.com>
2022-11-21 10:08:50 +0900

    feat : modal 페이지 기능 작업중

--- src/Row/index.js
+++ src/Row/index.js
@@ -1,18 +1,37 @@
-import React, {useState, useEffect} from "react";
-import axios from "axios.js";
-import { Container } from "./style";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp } from '@fortawesome/free-regular-svg-icons';
+import { faCircleChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { Container } from './style';
+import Modal from '../components/Modal';
 
-const base_url = "https://image.tmdb.org/t/p/original/";
+const base_url = 'https://image.tmdb.org/t/p/original/';
 
-const Row = ({title, fetchUrl, isLargeRow}) =>{
+const Row = ({ title, fetchUrl, isLargeRow }) => {
     const [movies, setMovies] = useState([]);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [movieTitle, setMovieTitle] = useState('');
+    const [movieImg, setMovieImg] = useState('');
+
+    const openModal = (title, largeImg, img) => {
+        setModalOpen(true);
+        setMovieTitle(title);
+        setMovieImg(isLargeRow ? largeImg : img);
+        console.log(title);
+    };
+    const closeModal = () => {
+        setModalOpen(false);
+    };
 
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
-            
-
             setMovies(request.data.results);
+            console.log(request);
+            // console.log(request.data.results[0].title);
             return request;
         }
         fetchData();
@@ -21,22 +40,47 @@ const Row = ({title, fetchUrl, isLargeRow}) =>{
     return (
         <Container>
             <h2>{title}</h2>
-  
-            <div className="rowLines">
-                {movies.map((movie) => (
-                    <img
-                        key={movie.id}
-                        className={`rowLineItem ${isLargeRow && "imgSizeLarge"}`}
-                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-                        alt={movie.name}
-                    />
+            <div className="row__posters">
+                {movies.map(movie => (
+                    <div className="content">
+                        <img
+                            key={movie.id}
+                            className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+                            src={`${base_url}${
+                                isLargeRow ? movie.poster_path : movie.backdrop_path
+                            }`}
+                            alt={movie.name}
+                        />
+                        <div className="icons">
+                            <span className="left">
+                                <FontAwesomeIcon icon={faCirclePlay} />
+                                <FontAwesomeIcon icon={faCirclePlus} />
+                                <FontAwesomeIcon icon={faThumbsUp} />
+                            </span>
+                            <span className="right">
+                                <FontAwesomeIcon
+                                    icon={faCircleChevronDown}
+                                    onClick={() => {
+                                        openModal(
+                                            movie.name,
+                                            movie.poster_path,
+                                            movie.backdrop_path,
+                                        );
+                                    }}
+                                />
+                            </span>
+                        </div>
+                    </div>
                 ))}
             </div>
-      </Container>
-  
-    )
-}
-
-
+            <Modal
+                open={modalOpen}
+                close={closeModal}
+                movieTitle={movieTitle}
+                img={movieImg}
+            ></Modal>
+        </Container>
+    );
+};
 
 export default Row;
