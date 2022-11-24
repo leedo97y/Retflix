4b01c9600abb4c039f6d03087aa49e3329274f28
채현 김 <75241542+ggongjukim@users.noreply.github.com>
2022-11-24 20:51:34 +0900

    Update: introHeader routing

--- src/components/IntroHeader/index.js
+++ src/components/IntroHeader/index.js
@@ -2,17 +2,14 @@
 import React from "react";
 import Logo from "assets/images/Logo.png";
 import {Headerdiv} from'./style'
+import {Link} from "react-router-dom"
 
-const handleToLogin =()=>{
-    console.log("로그인 페이지로 가게 할것");
-    alert("로그인 페이지 라우터")
-}
 
 const IntroHeader = ()=>{
     return (
         <Headerdiv>
             <div className="item logo"><img src = {Logo} alt="넷플릭스 로고"/></div>
-            <div className="item button"><button onClick ={handleToLogin}>로그인</button></div>
+            <div className="item button"><Link to="/login"><button>로그인</button></Link></div>
         </Headerdiv>
     )
 }
