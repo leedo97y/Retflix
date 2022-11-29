32216bffbf1d3b64ef3de0acbbe982e64aeb82eb
dongjooyun <dongjooyun@khu.ac.kr>
2022-11-30 04:13:47 +0900

    Update: 로그인 정보 저장 및 도움 element 생성

--- src/components/LoginContent/index.js
+++ src/components/LoginContent/index.js
@@ -25,11 +25,20 @@ const LoginContent = () => {
                             로그인
                         </button>
                     </div>
+                    <div className="LoginHelp">
+                        <div className="checkRemember">
+                            <input type="checkbox" id="checkR" value="true" />
+                            <label for="checkR">
+                                <span className="rememberMeText">로그인 정보 저장</span>
+                            </label>
+                        </div>
+                        <a href='/LoginHelp' className="LoginHelpLink">도움이 필요하신가요?</a>
+                    </div>
                 </div>
                 <div className="LoginSubText">
                     <div className="SignUpNow">
                         Netflix 회원이 아닌가요?
-                        <a href='/'>지금 가입하세요</a>
+                        <a href='/intro'>지금 가입하세요</a>
                     </div>
                     <div className="RecaptchaMsg">
                         <p>
@@ -43,7 +52,7 @@ const LoginContent = () => {
                             </button>
                         </p>
                         {!visible && <div className="RecaptchaToU">
-                            Google reCAPTCHA가 수집하는 정보에는 Google 개인정보처리방침과 서비스 약관이 적용되며, 해당 정보는 reCAPTCHA 서비스 제공, 관리 및 개선과 일반적인 보안 유지에 사용됩니다(Google의 개인 맞춤 광고에 사용 안 함).
+                            Google reCAPTCHA가 수집하는 정보에는 Google <br></br> <a href='https://policies.google.com/privacy'>개인정보처리방침</a>과 <a href='https://policies.google.com/terms'>서비스 약관</a>이 적용되며, 해당 정보는 reCAPTCHA 서비스 제공, 관리 및 개선과 일반적인 보안 <br></br> 유지에 사용됩니다(Google의 개인 맞춤 광고에 사용 안 함).
                         </div>}
                     </div>
                 </div>
--- src/components/LoginContent/style.js
+++ src/components/LoginContent/style.js
@@ -1,10 +1,12 @@
 // LoginContent
 import styled from "@emotion/styled";
+
 export const ContentStyle = styled.div`
     padding: 15px;
 
     .LoginContent {
         max-width: 314px;
+        min-height: 665px;
         margin: 0.5vh auto 8vh auto;
         background: rgba(0, 0, 0, 0.75);
         padding: 0px 68px 0px 68px;
@@ -61,7 +63,7 @@ export const ContentStyle = styled.div`
         font-size: 16px;
         line-height: 50px;
         width: 314px;
-        margin: 19px 68px 0 68px;
+        margin: 0px 68px 0 68px;
         padding: 0 20px;
     }
 
@@ -97,14 +99,65 @@ export const ContentStyle = styled.div`
         cursor: pointer;
         border-radius: 4px;
         border: none;
-        margin: 62px 68px;
+        margin: 40px 68px 10px 68px;
         padding: 16px;
         height: 48px;
         width: 314px;
     }
     
+    .LoginHelp {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    input[type="checkbox"] {
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+        background: #737373;
+        margin: 0;
+        border: none;
+        border-radius: 2px;
+        outline: 0;
+        width: 16px;
+        height: 16px;
+    }
+    
+    input[type="checkbox"]::after {
+        content: '✔';
+        padding-left: 3px;
+        display: none;
+    }
+
+    input[type="checkbox"]:checked {
+        /* background: #fff; */
+    }
+
+    input[type="checkbox"]:checked::after {
+        display: block;
+    }
+
+    .rememberMeText {
+        color: #b3b3b3;
+        font-size: 13px;
+        font-weight: 400;
+        margin-left: 5px;
+    }
+
+    .LoginHelpLink {
+        color: #b3b3b3;
+        font-size: 13px;
+        font-weight: 400;
+        text-decoration: none;
+    }
+
+    .LoginHelpLink:hover {
+        text-decoration: underline;
+    }
+
     .LoginSubText {
-        margin: 30px 0px auto 0px;
+        margin: 60px 0px auto 0px;
     }
 
     .SignUpNow {
@@ -152,8 +205,13 @@ export const ContentStyle = styled.div`
         color: #8c8c8c;
         font-size: 13px;
         text-align: left;
-        margin-bottom: 40px;
+        /* margin-bottom: 40px; */
         line-height: 16px;
+    }
+
+    .RecaptchaToU a {
+        color: #0071eb;
+        text-decoration: none;
     }
 `;
 
