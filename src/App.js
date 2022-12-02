6c381cafae0ebf1bd4e20d3fd626b3f648c9bd44
dongjooyun <dongjooyun@khu.ac.kr>
2022-12-02 15:35:39 +0900

    Update: route img foldername ,...

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import Login from 'pages/Login';
 import Main from 'pages/Main';
 import Intro from 'pages/Intro';
 import Video from 'pages/Video';
+import { useEffect } from 'react';
 import MoveProfile from 'pages/MoveProfile';
 
 
 function App() {
+    let token;
+    useEffect(() => {
+        token = sessionStorage.getItem("user");
 
-    const token = sessionStorage.getItem("user");
+    }, [token])
+
     return (
         <BrowserRouter basename="/">
             <Routes>
-                {token ? 
-                    (<Route path="/" element={<Main />} />)
-                    
+                {token ?
+                    (<Route exact path="/" element={<Main />} />)
+
                     :
-                    (<Route path="/" element={<Intro />} /> )
+                    (<Route exact path="/" element={<Intro />} />)
                 }
-                
+
                 <Route path="/login" element={<Login />} /> {/* => 로그인페이지 */}
                 <Route path="/intro" element={<Intro />} /> {/* => intro 페이지 */}
                 <Route path="/video" element={<Video />} /> {/* => video 페이지 */}
